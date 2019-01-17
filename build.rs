91bb53a0a38508c79a37c39a341173cfc6f6d387
Trip Volpe <trip.volpe@gmail.com>
2019-01-16 20:05:33 -0800

    Disable some extremely spammy C compiler warnings

--- build.rs
+++ build.rs
@@ -7,6 +7,10 @@ fn main() {
         .flag("-fno-rtti")
         .flag("-fno-exceptions")
         .flag("-O3")
+        // The C code is a dumpster fire, so disable a bunch of warnings
+        .flag("-Wno-implicit-fallthrough")
+        .flag("-Wno-shift-negative-value")
+        .flag("-Wno-array-bounds") // oh no
         // #defines
         .define("NDEBUG", None)
         .define("BLARGG_NONPORTABLE", None)
