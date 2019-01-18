cd0ba8a2227a37ba15a3e911fad3a3ea3d50eabd
Trip Volpe <trip.volpe@gmail.com>
2019-01-17 20:26:46 -0800

    Use flag_if_supported in build.rs so MSVC toolchain doesn't complain

--- build.rs
+++ build.rs
@@ -3,14 +3,14 @@ extern crate cc;
 fn main() {
     cc::Build::new()
         // Compilation options
-        .flag("-fPIC")
-        .flag("-fno-rtti")
-        .flag("-fno-exceptions")
-        .flag("-O3")
+        .flag_if_supported("-fPIC")
+        .flag_if_supported("-fno-rtti")
+        .flag_if_supported("-fno-exceptions")
+        .flag_if_supported("-O3")
         // The C code is a dumpster fire, so disable a bunch of warnings
-        .flag("-Wno-implicit-fallthrough")
-        .flag("-Wno-shift-negative-value")
-        .flag("-Wno-array-bounds") // oh no
+        .flag_if_supported("-Wno-implicit-fallthrough")
+        .flag_if_supported("-Wno-shift-negative-value")
+        .flag_if_supported("-Wno-array-bounds") // oh no
         // #defines
         .define("NDEBUG", None)
         .define("BLARGG_NONPORTABLE", None)
